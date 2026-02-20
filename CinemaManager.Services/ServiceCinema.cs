c46fbe5bb66d78c795a47cd5af4155b02d296f28
Dmytro Marych <dimamarich07@gmail.com>
2026-02-20 22:37:50 +0200

    Add ServiceCinema class for operations with fake storage

---
+++ CinemaManager.Services/ServiceCinema.cs
@@ -0,0 +1,40 @@
+﻿using CinemaManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaManager.Services
+{
+    public class CinemaService
+    {
+        public List<CinemaHallViewModel> GetAllHalls()
+        {
+            return FakeDataStore.CinemaHalls.Select(hall => new CinemaHallViewModel
+            {
+                Id = hall.Id,
+                Name = hall.Name,
+                SeatsNumber = hall.SeatsNumber,
+                Type = hall.Type
+            }).ToList();
+        }
+
+        public List<MovieSessionViewModel> GetSessionsByHall(int hallId)
+        {
+            return FakeDataStore.MovieSessions
+                .Where(session => session.CinemaHallId == hallId)
+                .Select(session => new MovieSessionViewModel
+                {
+                    Id = session.Id,
+                    CinemaHallId = session.CinemaHallId,
+                    MovieName = session.MovieName,
+                    Genre = session.Genre,
+                    ReleaseYear = session.ReleaseYear,
+                    StartTime = session.StartTime,
+                    DurationMinutes = session.DurationMinutes
+                }).ToList();
+        }
+
+    }
+}
